772b7be41adfe0a4969eca73f4a373e5d3ea8abb
abhitsahota <abhit.sahota@gmail.com>
2020-06-19 00:55:05 -0400

    env change

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -1,7 +1,7 @@
 const knex = require("knex");
 
 const knexfile = require("../knexfile.js");
-const environment = process.env.DB_ENV || 'development';
+const environment = process.env.DB_ENV || 'staging';
 
 console.log(environment)
 
